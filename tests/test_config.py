82233183501807d6756b398414a31582b69fdc31
Satyabrata Behera <sb22844@gmail.com>
2022-07-20 13:30:10 +0530

    created a min_max json

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,9 @@
+import Exception
 
+class NotInRange(Exception):
+    def __init__(self, message = "Value not in range"):
+        self.message = message
+        super().__init__(self.message)
 
 
 def test_generic():
